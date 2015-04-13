69da1474ffa09e0ab39f2f1046e250076b1e773c
Brian Huynh <brianhuynh1@gmail.com>
2015-04-13 14:15:53 -0700

    Added filter portion and updated UI

--- CameraTest/ViewController.swift
+++ CameraTest/ViewController.swift
@@ -6,6 +6,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var imageView: UIImageView!
     var newMedia: Bool?
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    var originalImage: UIImage!
+    
+    override func viewDidLoad() {
+        navigationController?.navigationBar.barTintColor = UIColor.blackColor()
+    }
     
     @IBAction func useCamera(sender: AnyObject) {
         
@@ -43,19 +50,58 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    @IBAction func changeFilter(sender: AnyObject) {
+        
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+        
+    }
+    
+    func setFilter(action: UIAlertAction!) {
+        
+        let beginImage = CIImage(image: originalImage)
+        context = CIContext(options: nil)
+        currentFilter = CIFilter(name: action.title)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+        
+    }
+    
+    func applyProcessing() {
+        
+        let inputKeys = currentFilter.inputKeys() as! [NSString]
+        if contains(inputKeys, kCIInputIntensityKey) { currentFilter.setValue(1, forKey: kCIInputIntensityKey) }
+        if contains(inputKeys, kCIInputRadiusKey) { currentFilter.setValue(200, forKey: kCIInputRadiusKey) }
+        if contains(inputKeys, kCIInputScaleKey) { currentFilter.setValue(10, forKey: kCIInputScaleKey) }
+        if contains(inputKeys, kCIInputCenterKey) { currentFilter.setValue(CIVector(x: imageView.image!.size.width / 2, y: imageView.image!.size.height / 2), forKey: kCIInputCenterKey) }
+        
+        let cgimg = context.createCGImage(currentFilter.outputImage, fromRect: currentFilter.outputImage.extent())
+        let processedImage = UIImage(CGImage: cgimg, scale: 1.0, orientation: UIImageOrientation.Right)
+        
+        self.imageView.image = processedImage
+    }
+    
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         
         let mediaType = info[UIImagePickerControllerMediaType] as! NSString
         
         self.dismissViewControllerAnimated(true, completion: nil)
-        let imageEditor = PhotoEditorViewController()
-        self.presentViewController(imageEditor, animated: true, completion: nil)
         
         if mediaType.isEqualToString(kUTTypeImage as String) {
             let image = info[UIImagePickerControllerOriginalImage]
                 as! UIImage
             
             imageView.image = image
+            originalImage = image
             
             if (newMedia == true) {
                 UIImageWriteToSavedPhotosAlbum(image, self,
